9121caaa65b94a92b429316a9e28c4c6733b6065
shivramomre <shivramomre@gmail.com>
2016-01-15 01:36:33 +0530

    Gtest with example

--- Learning/Learning/GoogleTest.cpp
+++ Learning/Learning/GoogleTest.cpp
@@ -1,36 +1,119 @@
-#include <gtest\gtest.h>
 #include <iostream>
+#include <gtest/gtest.h>
 
 using namespace std;
 
-class BankAccount
+struct BankAccount
 {
-public : 
 	int balance = 0;
 
 	BankAccount()
 	{
 	}
 
-	BankAccount(int balance)
+	explicit BankAccount(const int balance) : balance{ balance }
 	{
-		balance = balance;
 	}
 
-	void printData()
+	void deposite(int amount)
 	{
-		cout << "The balance is : " << balance;
+		balance += amount;
+	}
+
+	bool withdrawl(int amount)
+	{
+		if (amount <= balance)
+		{
+			balance -= amount;
+			return true;
+		}
+		else
+			return false;
 	}
 };
 
-TEST(AccountTest, BankAccountStartsEmpty)
+struct BankAccountTest : testing::Test
 {
-	BankAccount bank_account;
-	EXPECT_EQ(0, bank_account.balance);
+	BankAccount* account;
+
+	BankAccountTest()
+	{
+		account = new BankAccount();
+	}
+
+	~BankAccountTest()
+	{
+		delete account;
+	}
+};
+
+TEST_F(BankAccountTest, Test_account_when_empty)
+{
+	EXPECT_EQ(0, account->balance);
 }
+
+TEST_F(BankAccountTest, Can_deposit_amount)
+{
+	account->deposite(100);
+	EXPECT_EQ(100, account->balance);
+	account->deposite(20);
+	EXPECT_EQ(120, account->balance);
+}
+
+TEST_F(BankAccountTest, Can_withdrawl_amount)
+{
+	account->deposite(100);
+	EXPECT_EQ(100, account->balance);
+	account->withdrawl(50);
+	EXPECT_EQ(50, account->balance);
+	account->deposite(20);
+	EXPECT_EQ(70, account->balance);
+}
+
+
+struct Account_state
+{
+	int initial_balance;
+	int withdrawl_amount;
+	int final_balance;
+	bool success;
+
+	friend std::ostream& operator<<(std::ostream& os, const Account_state& obj)
+	{
+		return os
+			<< "initial_balance: " << obj.initial_balance
+			<< " withdrawl_amount: " << obj.withdrawl_amount
+			<< " final_balance: " << obj.final_balance
+			<< " success: " << obj.success;
+	}
+};
+
+struct AmountWithDrawlTest : BankAccountTest, testing::WithParamInterface<Account_state>
+{
+	AmountWithDrawlTest()
+	{
+		account->balance = GetParam().initial_balance;
+	}
+};
+
+TEST_P(AmountWithDrawlTest, Final_Balance)
+{
+	auto as = GetParam();
+	auto success = account->withdrawl(as.withdrawl_amount);
+	EXPECT_EQ(account->balance, as.final_balance);
+	EXPECT_EQ(success, as.success);
+}
+
+INSTANTIATE_TEST_CASE_P(Default, AmountWithDrawlTest,
+	testing::Values(
+		Account_state{ 100,50,50, true },
+		Account_state{ 100,20,80, true },
+		Account_state{ 100,200,100, false }
+	));
 
 int main(int argc, char* argv[])
 {
 	testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+	RUN_ALL_TESTS();
+	return 0;
 }
